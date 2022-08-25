9cd128111f4c36bf3cca26b99cc4d84037f69773
aaronjtonner <tonneraaron@gmail.com>
2022-08-25 11:58:06 -0600

    blog

--- src/pages/about.js
+++ src/pages/about.js
@@ -31,7 +31,7 @@ export default function About() {
       <Team />
       <Facts />
       <CTA
-        headline="your pivotal moment awaits..."
+        headline="Your pivotal moment awaits..."
         subhead="Take the Leap. We'll catch you!"
         description="Feeling insecure about your body can keep you from feeling confident and sexy. A boudoir photography session is a great way to start feeling more comfortable in your own skin. When you see yourself through a photographer's lens, beautiful and sexy, it will change the way you see yourself forever. You'll be able to look back at these photos years from now and still feel confident and empowered"
       />
