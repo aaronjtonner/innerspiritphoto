fffb78e6a6537bc9e30853560e341f1a3d5e0b96
aaronjtonner <tonneraaron@gmail.com>
2022-11-26 15:00:39 -0700

    padding

--- src/pages/thank-you.js
+++ src/pages/thank-you.js
@@ -1,10 +1,15 @@
 import React from "react"
 import Layout from "../components/layout"
-import { Container, Section } from "../components/layoutComponents"
+import {
+  Container,
+  HeroBannerPadding,
+  Section,
+} from "../components/layoutComponents"
 
 export default function ThankYou() {
   return (
     <Layout>
+      <HeroBannerPadding />
       <Section>
         <Container>
           <h1 className="headline">thank you for contacting us!</h1>
